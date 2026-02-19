4ddbde7e97e90f09d933bd3e1da01e5a9bfea431
Abid Hossain Sifat <itzabid006@gmail.com>
2026-02-19 16:21:37 +0600

    Add home

--- sript/login.js
+++ sript/login.js
@@ -1,1 +1,26 @@
 console.log ("Login functionality Coming")
+
+document.getElementById("Login").addEventListener("click", function(){
+
+    // Get number 
+    const Number = document.getElementById("input-number")
+    const Contact = Number.value;
+    console.log (Contact);
+
+    // Get Pin 
+    const pin = document.getElementById("input-pin");
+    const Pin = pin.value;
+    console.log (Pin);
+
+    // Condition 
+    if (Contact == "01234567890" && Pin == "1234"){
+        alert ("Login Successful")
+
+        window.location.assign("./Home.html")
+        // window.location.assign()
+    }
+    else {
+        alert ("Login Failed");
+        return;
+    }
+})
